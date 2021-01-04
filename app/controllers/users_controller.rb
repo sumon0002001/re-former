4985df954ef427d929bea0c688997b98e4eaae2f
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-05 00:52:05 +0600

    comment out entire html form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,15 @@ class UsersController < ApplicationController
   def create
     @user = User.new(username: params[:username], email: params[:email], password: params[:password])
 
-    if @user.save
+   if @user.save
       redirect_to new_user_path
     else
       render :new
-    end
+    end 
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
   end
 
 end
