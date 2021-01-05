476215d49c6ca4b36c1486fba09ec7be38014c47
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-06 01:38:33 +0600

    fixed errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,36 +2,36 @@ class UsersController < ApplicationController
   def index
     @user = User.all
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def create
     @user = User.new(user_params)
-  
+
     if @user.save
       redirect_to new_user_path
     else
       render :new
     end
   end
-  
+
   def edit
     @user = User.find(params[:id])
   end
 
   def update
-      @user = User.find(params[:id])
-      @user.update(user_params)
-      if @user.save
-        redirect_to new_user_path
-      else
-        render :edit
-      end
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :edit
+    end
   end
 
-    private
+  private
 
   def user_params
     params.require(:user).permit(:username, :password, :email)
