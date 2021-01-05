2915de5af0f245f3f9e955465bc935b392a5480f
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-05 22:47:25 +0600

    fixed linters error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-    validates :username, :presence => true, :uniqueness => true, :length => {:in => 3..20} 
-    validates :email, :presence => true, :uniqueness => true
-    validates :password, :presence => true
-
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
 end
