ca83fc9404ddce878e0fddef2a73426fc04a66af
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-01-06 01:35:18 +0600

    fixed linters errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,27 @@
 class UsersController < ApplicationController
-    def index
-      @user = User.all
+  def index
+    @user = User.all
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+  
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
   
-    def new
-      @user = User.new
-    end
-  
-    def create
-      @user = User.new(user_params)
-  
-      if @user.save
-        redirect_to new_user_path
-      else
-        render :new
-      end
-    end
-  
-    def destroy
-      @user = User.destroy(params[:id])
-      @user.save
-  
-      redirect_to users_path
-    end
-  
-    def edit
-      @user = User.find(params[:id])
-    end
-  
-    def update
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
       @user = User.find(params[:id])
       @user.update(user_params)
       if @user.save
@@ -36,12 +29,11 @@ class UsersController < ApplicationController
       else
         render :edit
       end
-    end
-  
-    private
-  
-    def user_params
-      params.require(:user).permit(:username, :password, :email)
-    end
   end
-  
+
+    private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email)
+  end
+end
